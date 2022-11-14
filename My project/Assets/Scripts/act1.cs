610618496b615efca09441bcce9d4e52195eda39
soytmen <114388813+soytmen@users.noreply.github.com>
2022-11-14 09:06:23 +0100

    1

---
+++ My project/Assets/Scripts/act1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class act1 : MonoBehaviour
+{
+    public float num = 3;
+
+    void Start()
+    {
+        
+    }
+
+
+}
